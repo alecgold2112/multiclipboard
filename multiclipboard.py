b2bc1e7936173215a9a5c4280fed804af47e1291
alecgold2112 <alecgoldstein2112@gmail.com>
2023-05-18 09:57:08 -0700

    Initial commit of multiclipboard.py

---
+++ multiclipboard.py
@@ -0,0 +1,48 @@
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+# Writes data into json file #
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+# Try to load file, if not return empty dictionary #
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    # Rewrites file to include data from key #
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard_paste()
+        save_data(SAVED_DATA, data)
+
+    # Loads key if available #
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+        else:
+            print("Key does not exist")
+
+    elif command == "list":
+        print(data)
+
+    else:
+        print("Unknown command")
+
+else:
+    print("Please pass exactly one command")
